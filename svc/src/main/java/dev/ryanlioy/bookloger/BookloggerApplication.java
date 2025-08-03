b20ba762bfa8d726697f4ac9c6857200533c2e3d
Ryan Lioy <rlioy1211@gmail.com>
2025-08-03 19:21:56 -0400

    create split build, move service to subproject

--- svc/src/main/java/dev/ryanlioy/bookloger/BookloggerApplication.java
+++ svc/src/main/java/dev/ryanlioy/bookloger/BookloggerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookloggerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BookloggerApplication.class, args);
 	}
-
 }
